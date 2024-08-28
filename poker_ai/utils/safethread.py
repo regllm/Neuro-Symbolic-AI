3a5589b936bad6757f6e1c5ea474903dde6d46d9
regllm <sonanh1234bitcoin@gmail.com>
2024-08-28 23:39:47 +0700

    update safethread multiprocessing function

--- poker_ai/utils/safethread.py
+++ poker_ai/utils/safethread.py
@@ -3,8 +3,107 @@ from typing import Optional, Iterable, Callable
 import ctypes
 import multiprocessing
 import time
+from multiprocessing.shared_memory import SharedMemory
 
+import numpy as np
 from tqdm import tqdm
+
+def multiprocess_ehs_calc(
+    source_iter: Iterable[any],
+    tasker: Callable[[Iterable[any], int, int], None],
+    result_size: int = None,
+):
+    result_size = result_size or len(source_iter)
+    result_bytes = result_size * 3 * 8
+    result_sm = SharedMemory(
+        name="result_sm", create=True, size=result_bytes
+    )
+    result = np.ndarray(
+        (result_size, 3), dtype=np.double, buffer=result_sm.buf
+    )
+
+    def process_all(batch, cursor, result_size):
+        sm = SharedMemory("result_sm")
+        result = np.ndarray(
+            (result_size, 3), dtype=np.double, buffer=sm.buf
+        )
+        tasker(batch, cursor, result)
+        sm.close()
+    
+    worker_count = multiprocessing.cpu_count()
+    batch_size = min(10_000, result_size // worker_count)
+    cursor = 0
+    max_batch_seconds = None
+    batch_failed = False
+    total = result_size
+    if total is None:
+        try:
+            total = len(source_iter)
+        except:
+            pass
+
+    with tqdm(total=total, ascii=" >=") as pbar:
+        while True:
+            task_done = False
+            
+            if batch_failed:
+                batches = cached_batches
+            else:
+                batches = []
+                for _ in range(worker_count):
+                    batch = []
+                    for _ in range(batch_size):
+                        try:
+                            batch.append(next(source_iter))
+                        except StopIteration:
+                            task_done = True
+                            break
+                    if len(batch) > 0:
+                        batches.append(batch)
+            
+            cached_batches = batches
+            batch_failed = False
+            
+            total_batch_size = 0
+            processes = []
+
+            start = time.time()
+            for batch in batches:
+                process = multiprocessing.Process(
+                    target=process_all, args=(batch, cursor, result_size)
+                )
+                process.start()
+                processes.append(process)
+                total_batch_size += len(batch)
+                cursor += len(batch)
+        
+            for process in processes:
+                if max_batch_seconds is None:
+                    process.join()
+                    continue
+                process.join(timeout=max_batch_seconds)
+
+                if process.is_alive():
+                    # Failed to handle this batch.
+                    batch_failed = True
+                    cursor -= total_batch_size
+                    break
+
+            if batch_failed:
+                continue
+
+            end = time.time()
+            if max_batch_seconds is None:
+                duration = end - start
+                max_batch_seconds = int(duration * 3)
+                
+            pbar.update(total_batch_size)
+            
+            if task_done:
+                break
+    
+    return result, result_sm
+
 
 def batch_process(
     source_iter: Iterable[any],
