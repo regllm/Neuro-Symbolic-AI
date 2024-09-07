3ac547418384274e61d14f623815fb5978d13c28
regllm <sonanh1234bitcoin@gmail.com>
2024-09-07 11:03:49 +0700

    handle full deck

--- rust_poker_ai/src/main.rs
+++ rust_poker_ai/src/main.rs
@@ -273,8 +273,8 @@ fn build_flop_lut(
 
 
 fn build_lut() {
-    let deck = combo::create_deck(2, 4);
-    // let deck = combo::create_deck(2, 14);
+    // let deck = combo::create_deck(2, 4);
+    let deck = combo::create_deck(2, 14);
     let start_combos = combo::create_card_combos(&deck, 2);
     let lookup = card::load_lookup("./assets/lookup.json");
 
