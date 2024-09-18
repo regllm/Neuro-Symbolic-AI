d5b4a440abe0787271ec498ca664a2d500f7caa5
regllm <sonanh1234bitcoin@gmail.com>
2024-09-19 03:06:51 +0700

    support u8 clustering and distance calc

--- rust_poker_ai/src/distance.rs
+++ rust_poker_ai/src/distance.rs
@@ -1,13 +1,13 @@
 use emd::distance;
 use ndarray::Array1;
-use num_traits::Num;
+use num_traits::{Num, ToPrimitive};
 
 
-pub fn wasserstein<T: Num>(v1: &Vec<T>, v2: &Vec<T>) -> f64 {
+pub fn wasserstein<T: Num + ToPrimitive, T2: Num + ToPrimitive>(v1: &Vec<T>, v2: &Vec<T2>) -> f64 {
     // Ensure both vectors have the same length
     assert_eq!(v1.len(), v2.len(), "Input vectors must have the same length");
 
-    let arr1: Array1<f64> = v1.iter().map(|&x| x.into()).collect::<Array1<f64>>();
-    let arr2: Array1<f64> = v2.iter().map(|&x| x.into()).collect::<Array1<f64>>();
+    let arr1: Array1<f64> = v1.iter().map(|x| x.to_f64().unwrap()).collect::<Array1<f64>>();
+    let arr2: Array1<f64> = v2.iter().map(|x| x.to_f64().unwrap()).collect::<Array1<f64>>();
     distance(&arr1.view(), &arr2.view()) as f64
 }
