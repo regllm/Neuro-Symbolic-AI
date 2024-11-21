8fd237e3bc40ce859c0913c4d780d2493e8cb89a
regllm <sonanh1234bitcoin@gmail.com>
2024-11-21 19:01:20 +0700

    use file-based card combos

--- poker_ai/clustering/card_combos.py
+++ poker_ai/clustering/card_combos.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import math
 from pathlib import Path
 from typing import List
@@ -47,48 +48,51 @@ class CardCombos:
         card_combos_river_filename = f"card_combos_river_{low_card_rank}_to_{high_card_rank}.joblib"
         self.card_combos_river_path: Path = Path(save_dir) / card_combos_river_filename
 
-        card_combos_flop_txt_filename = f"card_combos_flop_txt_{low_card_rank}_to_{high_card_rank}.csv"
-        self.card_combos_flop_txt_path: Path = Path(save_dir) / card_combos_flop_txt_filename
-        card_combos_turn_txt_filename = f"card_combos_turn_txt_{low_card_rank}_to_{high_card_rank}.csv"
-        self.card_combos_turn_txt_path: Path = Path(save_dir) / card_combos_turn_txt_filename
-        card_combos_river_txt_filename = f"card_combos_river_txt_{low_card_rank}_to_{high_card_rank}.csv"
-        self.card_combos_river_txt_path: Path = Path(save_dir) / card_combos_river_txt_filename
+        card_combos_flop_csv_filename = f"card_combos_flop_csv_{low_card_rank}_to_{high_card_rank}.csv"
+        self.card_combos_flop_csv_path: Path = Path(save_dir) / card_combos_flop_csv_filename
+        card_combos_turn_csv_filename = f"card_combos_turn_csv_{low_card_rank}_to_{high_card_rank}.csv"
+        self.card_combos_turn_csv_path: Path = Path(save_dir) / card_combos_turn_csv_filename
+        card_combos_river_csv_filename = f"card_combos_river_csv_{low_card_rank}_to_{high_card_rank}.csv"
+        self.card_combos_river_csv_path: Path = Path(save_dir) / card_combos_river_csv_filename
 
-        try:
-            self.flop = joblib.load(self.card_combos_flop_path)
-            log.info("loaded flop")
-            with open(card_combos_flop_txt_filename, "w") as f:
-                for row in tqdm(self.flop, ascii=" >="):
+        if os.path.exists(self.card_combos_flop_path) and not os.path.exists(self.card_combos_flop_csv_path):
+            flop = joblib.load(self.card_combos_flop_path)
+            log.info("converting flop")
+            with open(card_combos_flop_csv_filename, "w") as f:
+                for row in tqdm(flop, ascii=" >="):
                     f.write(",".join([str(int(x)) for x in row]) + "\n")
-            log.info("converted flop")
-        except FileNotFoundError:
-            self.flop = self.create_info_combos(
-                self.starting_hands, 3
-            )
-            joblib.dump(self.flop, self.card_combos_flop_path)
+            os.remove(self.card_combos_flop_path)
+        elif not os.path.exists(self.card_combos_flop_csv_path):
+            self.write_info_combos(self.starting_hands, 3, card_combos_flop_csv_filename)
             log.info("created flop")
-        try:
-            self.turn = joblib.load(self.card_combos_turn_path)
-            log.info("loaded turn")
-            # np.savetxt(card_combos_turn_txt_filename, self.turn)
-            # log.info("converted turn")
-        except FileNotFoundError:
-            self.turn = self.create_info_combos(
-                self.starting_hands, 4
-            )
-            joblib.dump(self.turn, self.card_combos_turn_path)
+        else:
+            log.info("using pre-written flop")
+
+        if os.path.exists(self.card_combos_turn_path) and not os.path.exists(self.card_combos_turn_csv_path):
+            turn = joblib.load(self.card_combos_turn_path)
+            log.info("converting turn")
+            with open(card_combos_turn_csv_filename, "w") as f:
+                for row in tqdm(turn, ascii=" >="):
+                    f.write(",".join([str(int(x)) for x in row]) + "\n")
+            os.remove(self.card_combos_turn_path)
+        elif not os.path.exists(self.card_combos_turn_csv_path):
+            self.write_info_combos(self.starting_hands, 3, card_combos_turn_csv_filename)
             log.info("created turn")
-        try:
-            self.river = joblib.load(self.card_combos_river_path)
-            log.info("loaded river")
-            # np.savetxt(card_combos_river_txt_filename, self.river)
-            # log.info("converted river")
-        except FileNotFoundError:
-            self.river = self.create_info_combos(
-                self.starting_hands, 5
-            )
-            joblib.dump(self.river, self.card_combos_river_path)
+        else:
+            log.info("using pre-written turn")
+
+        if os.path.exists(self.card_combos_river_path) and not os.path.exists(self.card_combos_river_csv_path):
+            river = joblib.load(self.card_combos_river_path)
+            log.info("converting river")
+            with open(card_combos_river_csv_filename, "w") as f:
+                for row in tqdm(river, ascii=" >="):
+                    f.write(",".join([str(int(x)) for x in row]) + "\n")
+            os.remove(self.card_combos_river_path)
+        elif not os.path.exists(self.card_combos_river_csv_path):
+            self.write_info_combos(self.starting_hands, 3, card_combos_river_csv_filename)
             log.info("created river")
+        else:
+            log.info("using pre-written river")
 
     def get_card_combos(
         self, num_cards: int
@@ -108,6 +112,42 @@ class CardCombos:
         combos = np.array([c for c in combinations(self._sorted_cards, num_cards)])
 
         return combos
+
+    def write_info_combos(
+        self, start_combos: np.ndarray, public_num_cards: int, output_path: str
+    ):
+        if public_num_cards == 3:
+            betting_stage = "flop"
+        elif public_num_cards == 4:
+            betting_stage = "turn"
+        elif public_num_cards == 5:
+            betting_stage = "river"
+        else:
+            betting_stage = "unknown"
+        
+        max_count = len(start_combos) * math.comb(len(self._cards) - len(start_combos[0]), public_num_cards)
+        hand_size = len(start_combos[0]) + public_num_cards
+
+        with open(output_path, "w") as f:
+            for start_combo in tqdm(
+                start_combos,
+                dynamic_ncols=True,
+                desc=f"Creating {betting_stage} info combos into a file",
+                ascii=" >=",
+            ):
+                publics = np.array(
+                    [
+                        c for c in combinations(
+                            [c for c in self._sorted_cards if c not in start_combo],
+                            public_num_cards,
+                        )
+                    ]
+                )
+                for public_combo in publics:
+                    f.write(",".join([str(int(x)) for x in start_combo[::-1]]))
+                    f.write(",")
+                    f.write(",".join([str(int(x)) for x in public_combo[::-1]]))
+                    f.write("\n")
 
     def create_info_combos(
         self, start_combos: np.ndarray, public_num_cards: int
