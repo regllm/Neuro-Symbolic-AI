34b7a94b9bad416f4296c6d2ed5afc180991741d
regllm <sonanh1234bitcoin@gmail.com>
2024-11-26 19:01:24 +0700

    reorder code

--- poker_ai/clustering/card_info_lut_builder.py
+++ poker_ai/clustering/card_info_lut_builder.py
@@ -171,17 +171,17 @@ class CardInfoLutBuilder(CardCombos):
                 cursor = 0
                 while True:
                     done = False
+                    river_batch = []
+                    for _ in range(batch_size):
+                        try:
+                            river_batch.append(next(self.river))
+                        except StopIteration:
+                            done = True
+                            break
+                    curr_batch_size = len(river_batch)
+                    print("RIVER BATCH")
+                    print(river_batch)
                     with concurrent.futures.ProcessPoolExecutor() as executor:
-                        river_batch = []
-                        for _ in range(batch_size):
-                            try:
-                                river_batch.append(next(self.river))
-                            except StopIteration:
-                                done = True
-                                break
-                        curr_batch_size = len(river_batch)
-                        print("RIVER BATCH")
-                        print(river_batch)
                         batch_result = executor.map(
                             self.process_river_ehs, river_batch, chunksize=9600
                         )
