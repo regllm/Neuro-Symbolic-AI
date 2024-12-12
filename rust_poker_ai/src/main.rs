2d003530a5337199acd5538062444650cc2ef73a
regllm <sonanh1234bitcoin@gmail.com>
2024-12-12 19:01:39 +0700

    implement simulate flop

--- rust_poker_ai/src/main.rs
+++ rust_poker_ai/src/main.rs
@@ -5,100 +5,74 @@ mod distance;
 mod math;
 mod shuffle;
 mod strength;
+// mod test;
 
 use indicatif::{ProgressBar, ProgressStyle};
 use std::time::{Instant, Duration};
 use std::fs::File;
 use std::io::{self, Write};
-use rand::seq::SliceRandom;
-use ndarray::array;
 
 
+const FLOP_SIMULATION_COUNT: u32 = 6;
 const TURN_SIMULATION_COUNT: u32 = 6;
 const RIVER_SIMULATION_COUNT: u32 = 6;
+const FLOP_CLUSTER_COUNT: u32 = 50;
+const TURN_CLUSTER_COUNT: u32 = 50;
 const RIVER_CLUSTER_COUNT: u32 = 50;
 
 
-fn test() {
-    // let target: Vec<Vec<f64>> = vec![
-    //     vec![0.0f64, 0.1f64, 0.0f64, 0.0f64],
-    //     vec![0.1f64, 0.1f64, 0.0f64, 0.0f64],
-    //     vec![0.0f64, 0.1f64, 0.1f64, 0.0f64],
-    //     vec![2.0f64, 2.1f64, 2.0f64, 0.0f64],
-    //     vec![2.1f64, 2.1f64, 2.0f64, 0.0f64],
-    //     vec![2.0f64, 2.1f64, 2.1f64, 0.0f64],
-    // ];
+fn save_combos(combos: &Vec<Vec<i32>>, file_path: &str) {
+    let style = ProgressStyle::default_bar().template("[{elapsed_precise}] {bar:40.cyan/blue} {pos}/{len} ({eta} left)").unwrap();
+    let progress = ProgressBar::new(combos.len() as u64);
+    progress.set_style(style);
 
-    // let encoded: Vec<u8> = bincode::serialize(&target).unwrap();
+    let mut file = File::create(file_path).unwrap();
+    for row in combos.iter() {
+        let row_str = row.iter().map(|&x| x.to_string()).collect::<Vec<String>>().join(",");
+        writeln!(file, "{}", row_str).unwrap();
+        progress.inc(1);
+    }
 
-    // let mut file = File::create(filename);
-    // file.write_all(&encoded);
-
-    // let decoded: Vec<Vec<f64>> = bincode::deserialize(&encoded[..]).unwrap();
-    // assert_eq!(target, decoded);
-    
-    
-    // let result = card::new(14, 's');
-    // println!("Card representation for Spade A: {}", result);
-
-    let p = vec![0.1, 0.2, 0.3, 0.4];
-    let q = vec![0.2, 0.3, 0.2, 0.3];
-
-    // Calculate Wasserstein distance
-    let distance = distance::wasserstein(&p, &q);
-    println!("Wasserstein distance: {}", distance);
-
-    let deck = combo::create_deck(2, 4);
-    
-    // let deck = combo::create_deck(2, 14);
-    let start_combos = combo::create_card_combos(&deck, 2);
-
-    // let flop_start_time = Instant::now();
-    // let flop_combos = combo::create_info_combos(&deck, &start_combos, 3);
-    // let flop_end_time = Instant::now();
-    // let flop_elapsed_time = flop_end_time - flop_start_time;
-    // println!("Created Flop combos in {:?} seconds.", flop_elapsed_time);
-
-    let turn_start_time = Instant::now();
-    let turn_combos = combo::create_info_combos(&deck, &start_combos, 4);
-    let turn_end_time = Instant::now();
-    let turn_elapsed_time = turn_end_time - turn_start_time;
-    println!("Created Turn combos in {:?}.", turn_elapsed_time);
-
-    let river_start_time = Instant::now();
-    let river_combos = combo::create_info_combos(&deck, &start_combos, 5);
-    let river_end_time = Instant::now();
-    let river_elapsed_time = river_end_time - river_start_time;
-    println!("Created River combos in {:?}.", river_elapsed_time);
-
-    let lookup = card::load_lookup("./assets/lookup.json");
-
-    let river_simulate_start_time = Instant::now();
-    let result = strength::simulate_river_hand_strengths(
-        &deck,
-        &river_combos,
-        &lookup,
-        RIVER_SIMULATION_COUNT,
-    );
-    let river_simulate_end_time = Instant::now();
-    let river_simulate_elapsed_time = 
-        river_simulate_end_time - river_simulate_start_time;
-    println!("Simulated River hand strengths in {:?}.", river_simulate_elapsed_time);
-
-    let (centers, clusters) = cluster::kmeans(
-        &result,
-        RIVER_CLUSTER_COUNT,
-    );
-
-    println!("Kmeans centers: {:?}", centers);
-    println!("Kmeans clusters: {:?}", clusters);
+    progress.finish();
 }
+
+
+fn save_centroids(centroids: &Vec<Vec<f64>>, file_path: &str) {
+    let style = ProgressStyle::default_bar().template("[{elapsed_precise}] {bar:40.cyan/blue} {pos}/{len} ({eta} left)").unwrap();
+    let progress = ProgressBar::new(centroids.len() as u64);
+    progress.set_style(style);
+
+    let mut file = File::create(file_path).unwrap();
+    for row in centroids.iter() {
+        let row_str = row.iter().map(|&x| x.to_string()).collect::<Vec<String>>().join(",");
+        writeln!(file, "{}", row_str).unwrap();
+        progress.inc(1);
+    }
+
+    progress.finish();
+}
+
+
+fn save_clusters(clusters: &Vec<u32>, file_path: &str) {
+    let style = ProgressStyle::default_bar().template("[{elapsed_precise}] {bar:40.cyan/blue} {pos}/{len} ({eta} left)").unwrap();
+    let progress = ProgressBar::new(clusters.len() as u64);
+    progress.set_style(style);
+
+    let mut file = File::create(file_path).unwrap();
+    for row in clusters.iter() {
+        writeln!(file, "{}", row.to_string()).unwrap();
+        progress.inc(1);
+    }
+
+    progress.finish();
+}
+
 
 fn build_river_lut(
     deck: &Vec<i32>,
     start_combos: &Vec<Vec<i32>>,
     lookup: &card::LookupTable,
-) {
+) -> (Vec<Vec<f64>>, Vec<u32>) {
     println!("Creating River combos.");
     let mut start_time = Instant::now();
     let river_combos = combo::create_info_combos(&deck, &start_combos, 5);
@@ -133,35 +107,145 @@ fn build_river_lut(
     elapsed_time = Instant::now() - start_time;
     println!("Clustered River combos in {:?}.", elapsed_time);
 
-    let style = ProgressStyle::default_bar().template("[{elapsed_precise}] {bar:40.cyan/blue} {pos}/{len} ({eta} left)").unwrap();
-
-    println!("Writing River centroids.");
-    let write_centroid_progress = ProgressBar::new(centroids.len() as u64);
-    write_centroid_progress.set_style(style.clone());
+    println!("Writing River combos.");
     start_time = Instant::now();
-    let mut centroids_file = File::create("./output/river_centroids.txt").unwrap();
-    for row in centroids.iter() {
-        let row_str = row.iter().map(|&x| x.to_string()).collect::<Vec<String>>().join(",");
-        writeln!(centroids_file, "{}", row_str).unwrap();
-        write_centroid_progress.inc(1);
-    }
-    write_centroid_progress.finish();
+    save_combos(&river_combos, "./output/river_combos.txt");
+    elapsed_time = Instant::now() - start_time;
+    println!("Wrote River combos in {:?}.", elapsed_time);
+    
+    println!("Writing River centroids.");
+    start_time = Instant::now();
+    save_centroids(&centroids, "./output/river_centroids.txt");
     elapsed_time = Instant::now() - start_time;
     println!("Wrote River centroids in {:?}.", elapsed_time);
     
     println!("Writing River clusters.");
-    let write_clusters_progress = ProgressBar::new(clusters.len() as u64);
-    write_clusters_progress.set_style(style.clone());
     start_time = Instant::now();
-    let mut clusters_file = File::create("./output/river_clusters.txt").unwrap();
-    for row in clusters.iter() {
-        writeln!(clusters_file, "{}", row.to_string()).unwrap();
-        write_clusters_progress.inc(1);
-    }
-    write_clusters_progress.finish();
+    save_clusters(&clusters, "./output/river_clusters.txt");
     elapsed_time = Instant::now() - start_time;
     println!("Wrote River clusters in {:?}.", elapsed_time);
+
+    (centroids, clusters)
 }
+
+
+fn build_turn_lut(
+    deck: &Vec<i32>,
+    start_combos: &Vec<Vec<i32>>,
+    lookup: &card::LookupTable,
+    river_centroids: &Vec<Vec<f64>>,
+) -> (Vec<Vec<f64>>, Vec<u32>) {
+    println!("Creating Turn combos.");
+    let mut start_time = Instant::now();
+    let turn_combos = combo::create_info_combos(&deck, &start_combos, 4);
+    let mut elapsed_time = Instant::now() - start_time;
+    println!("Created Turn combos in {:?}.", elapsed_time);
+
+    println!("Simulating Turn hand strengths.");
+    start_time = Instant::now();
+    let result = strength::simulate_turn_hand_strengths(
+        deck,
+        &turn_combos,
+        lookup,
+        river_centroids,
+        RIVER_SIMULATION_COUNT,
+        TURN_SIMULATION_COUNT,
+        RIVER_CLUSTER_COUNT,
+    );
+    elapsed_time = Instant::now() - start_time;
+    println!("Simulated Turn hand strengths in {:?}.", elapsed_time);
+
+    println!("Clustering Turn combos.");
+    start_time = Instant::now();
+    let (centroids, clusters) = cluster::kmeans(
+        &result,
+        TURN_CLUSTER_COUNT,
+    );
+    elapsed_time = Instant::now() - start_time;
+    println!("Clustered Turn combos in {:?}.", elapsed_time);
+
+    println!("Writing Turn combos.");
+    start_time = Instant::now();
+    save_combos(&turn_combos, "./output/turn_combos.txt");
+    elapsed_time = Instant::now() - start_time;
+    println!("Wrote Turn combos in {:?}.", elapsed_time);
+
+    println!("Writing Turn centroids.");
+    start_time = Instant::now();
+    save_centroids(&centroids, "./output/turn_centroids.txt");
+    elapsed_time = Instant::now() - start_time;
+    println!("Wrote Turn centroids in {:?}.", elapsed_time);
+    
+    println!("Writing Turn clusters.");
+    start_time = Instant::now();
+    save_clusters(&clusters, "./output/turn_clusters.txt");
+    elapsed_time = Instant::now() - start_time;
+    println!("Wrote Turn clusters in {:?}.", elapsed_time);
+
+    (centroids, clusters)
+}
+
+
+fn build_flop_lut(
+    deck: &Vec<i32>,
+    start_combos: &Vec<Vec<i32>>,
+    lookup: &card::LookupTable,
+    river_centroids: &Vec<Vec<f64>>,
+    turn_centroids: &Vec<Vec<f64>>,
+) -> (Vec<Vec<f64>>, Vec<u32>) {
+    println!("Creating Flop combos.");
+    let mut start_time = Instant::now();
+    let flop_combos = combo::create_info_combos(&deck, &start_combos, 3);
+    let mut elapsed_time = Instant::now() - start_time;
+    println!("Created Flop combos in {:?}.", elapsed_time);
+
+    println!("Simulating Turn hand strengths.");
+    start_time = Instant::now();
+    let result = strength::simulate_flop_hand_strengths(
+        deck,
+        &flop_combos,
+        lookup,
+        river_centroids,
+        turn_centroids,
+        RIVER_SIMULATION_COUNT,
+        TURN_SIMULATION_COUNT,
+        FLOP_SIMULATION_COUNT,
+        RIVER_CLUSTER_COUNT,
+        TURN_CLUSTER_COUNT,
+    );
+    elapsed_time = Instant::now() - start_time;
+    println!("Simulated Turn hand strengths in {:?}.", elapsed_time);
+
+    println!("Clustering Flop combos.");
+    start_time = Instant::now();
+    let (centroids, clusters) = cluster::kmeans(
+        &result,
+        FLOP_CLUSTER_COUNT,
+    );
+    elapsed_time = Instant::now() - start_time;
+    println!("Clustered Flop combos in {:?}.", elapsed_time);
+
+    println!("Writing Flop combos.");
+    start_time = Instant::now();
+    save_combos(&flop_combos, "./output/flop_combos.txt");
+    elapsed_time = Instant::now() - start_time;
+    println!("Wrote Flop combos in {:?}.", elapsed_time);
+
+    println!("Writing Flop centroids.");
+    start_time = Instant::now();
+    save_centroids(&centroids, "./output/flop_centroids.txt");
+    elapsed_time = Instant::now() - start_time;
+    println!("Wrote Flop centroids in {:?}.", elapsed_time);
+    
+    println!("Writing Flop clusters.");
+    start_time = Instant::now();
+    save_clusters(&clusters, "./output/flop_clusters.txt");
+    elapsed_time = Instant::now() - start_time;
+    println!("Wrote Flop clusters in {:?}.", elapsed_time);
+
+    (centroids, clusters)
+}
+
 
 fn build_lut() {
     // let deck = combo::create_deck(2, 4);
@@ -169,11 +253,28 @@ fn build_lut() {
     let start_combos = combo::create_card_combos(&deck, 2);
     let lookup = card::load_lookup("./assets/lookup.json");
 
-    build_river_lut(&deck, &start_combos, &lookup);
+    let (river_centroids, river_clusters) = build_river_lut(
+        &deck,
+        &start_combos,
+        &lookup,
+    );
+    let (turn_centroids, turn_clusters) = build_turn_lut(
+        &deck,
+        &start_combos,
+        &lookup,
+        &river_centroids,
+    );
+    let (flop_centroids, flop_clusters) = build_flop_lut(
+        &deck,
+        &start_combos,
+        &lookup,
+        &river_centroids,
+        &turn_centroids,
+    );
 }
 
 
 fn main() {
-    // test();
+    // test::test();
     build_lut();
 }
---
+++ rust_poker_ai/src/test.rs
@@ -0,0 +1,77 @@
+use std::time::{Instant, Duration};
+
+
+fn test() {
+    // let target: Vec<Vec<f64>> = vec![
+    //     vec![0.0f64, 0.1f64, 0.0f64, 0.0f64],
+    //     vec![0.1f64, 0.1f64, 0.0f64, 0.0f64],
+    //     vec![0.0f64, 0.1f64, 0.1f64, 0.0f64],
+    //     vec![2.0f64, 2.1f64, 2.0f64, 0.0f64],
+    //     vec![2.1f64, 2.1f64, 2.0f64, 0.0f64],
+    //     vec![2.0f64, 2.1f64, 2.1f64, 0.0f64],
+    // ];
+
+    // let encoded: Vec<u8> = bincode::serialize(&target).unwrap();
+
+    // let mut file = File::create(filename);
+    // file.write_all(&encoded);
+
+    // let decoded: Vec<Vec<f64>> = bincode::deserialize(&encoded[..]).unwrap();
+    // assert_eq!(target, decoded);
+    
+    
+    // let result = card::new(14, 's');
+    // println!("Card representation for Spade A: {}", result);
+
+    let p = vec![0.1, 0.2, 0.3, 0.4];
+    let q = vec![0.2, 0.3, 0.2, 0.3];
+
+    // Calculate Wasserstein distance
+    let distance = distance::wasserstein(&p, &q);
+    println!("Wasserstein distance: {}", distance);
+
+    let deck = combo::create_deck(2, 4);
+    
+    // let deck = combo::create_deck(2, 14);
+    let start_combos = combo::create_card_combos(&deck, 2);
+
+    // let flop_start_time = Instant::now();
+    // let flop_combos = combo::create_info_combos(&deck, &start_combos, 3);
+    // let flop_end_time = Instant::now();
+    // let flop_elapsed_time = flop_end_time - flop_start_time;
+    // println!("Created Flop combos in {:?} seconds.", flop_elapsed_time);
+
+    let turn_start_time = Instant::now();
+    let turn_combos = combo::create_info_combos(&deck, &start_combos, 4);
+    let turn_end_time = Instant::now();
+    let turn_elapsed_time = turn_end_time - turn_start_time;
+    println!("Created Turn combos in {:?}.", turn_elapsed_time);
+
+    let river_start_time = Instant::now();
+    let river_combos = combo::create_info_combos(&deck, &start_combos, 5);
+    let river_end_time = Instant::now();
+    let river_elapsed_time = river_end_time - river_start_time;
+    println!("Created River combos in {:?}.", river_elapsed_time);
+
+    let lookup = card::load_lookup("./assets/lookup.json");
+
+    let river_simulate_start_time = Instant::now();
+    let result = strength::simulate_river_hand_strengths(
+        &deck,
+        &river_combos,
+        &lookup,
+        RIVER_SIMULATION_COUNT,
+    );
+    let river_simulate_end_time = Instant::now();
+    let river_simulate_elapsed_time = 
+        river_simulate_end_time - river_simulate_start_time;
+    println!("Simulated River hand strengths in {:?}.", river_simulate_elapsed_time);
+
+    let (centers, clusters) = cluster::kmeans(
+        &result,
+        RIVER_CLUSTER_COUNT,
+    );
+
+    println!("Kmeans centers: {:?}", centers);
+    println!("Kmeans clusters: {:?}", clusters);
+}
