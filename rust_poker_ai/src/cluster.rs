4b381005fc5061a156bbb7229833bbf2da2556a4
regllm <sonanh1234bitcoin@gmail.com>
2024-12-20 19:01:48 +0700

    support u8 clustering and distance calc

--- rust_poker_ai/src/cluster.rs
+++ rust_poker_ai/src/cluster.rs
@@ -1,11 +1,11 @@
-use ckmeans::cluster_with_kmeans;
+use ckmeans::cluster_u8;
 const MAX_ITER: u32 = 300;
 
 
 pub fn kmeans(
-    data_points: &Vec<Vec<f64>>,
+    data_points: &Vec<Vec<u8>>,
     cluster_count: u32,
 ) -> (Vec<Vec<f64>>, Vec<u32>) {
-    let (centers, clusters) = cluster_with_kmeans(data_points, cluster_count, MAX_ITER);
+    let (centers, clusters) = cluster_u8(data_points, cluster_count, MAX_ITER);
     (centers, clusters)
 }
