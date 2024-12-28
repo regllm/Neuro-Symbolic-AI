442d7bd1a9ac7f05b4a33655b251f311d133665f
regllm <sonanh1234bitcoin@gmail.com>
2024-12-28 19:01:54 +0700

    temporarily print distance calculation

--- rust_poker_ai/src/distance.rs
+++ rust_poker_ai/src/distance.rs
@@ -16,6 +16,8 @@ pub fn wasserstein(v1: &Vec<f32>, v2: &Vec<f64>) -> f32 {
 
     let v2_f32: Vec<f32> = v2.iter().map(|&x| x as f32).collect();
 
+    println!("distance: {:?}, {:?}", v1, v2_f32);
+
     let distance = distance(&v1, &v2_f32) as f32;
 
     drop(v2_f32);
